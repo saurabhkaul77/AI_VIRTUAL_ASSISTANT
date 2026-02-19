b53ae47f63f01c2ec4e658826ac691d3ba7699af
saurabhkaul77 <77skaul@gmail.com>
2026-02-19 20:55:28 +0530

    start frontend

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,4 +1,75 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import genToken from "../config/token.js"
 
 export const signUp = async (req, res) => {
-    
+    try {
+        const{name, email, password} = req.body
+
+        const existEmail = await User.findOne({email})
+        if(existEmail){
+            return res.status(400).json({message: "Email already exist"})
+        }
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const token = await genToken(user._id)
+        res.cookie("token", token,{
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            secure: false
+        })
+
+        return res.status(201).json(user)
+    } catch (error) {
+        return res.status(500).json({message: `Sign up error: ${error}`})
+        
+    }
+}
+
+
+export const login = async (req, res) => {
+    try {
+        const{email, password} = req.body
+
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message: "Email does not exist"})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({message: "Password incorrect"})
+        }                                
+
+        const token = await genToken(user._id)
+        res.cookie("token", token,{
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            secure: false
+        })
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({message: `Login error: ${error}`})        
+    }
+}
+
+
+export const logout = async (req, res)=>{
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message: "Logout successful"})
+    } catch (error) {
+        return res.status(500).json({message: `Logout error: ${error}`})        
+    }
 }
---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { login, logout, signUp } from "../controllers/auth.controllers.js"
+
+const authRouter = express.Router()
+
+authRouter.post("/signup", signUp)
+authRouter.post("/signin", login)
+authRouter.get("/logout", logout)
+
+
+export default authRouter
