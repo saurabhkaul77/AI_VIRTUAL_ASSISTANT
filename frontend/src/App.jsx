827aab34ff312ffed5590ac4eb2843df3e8cbbfe
Saurabh Kaul <77skaul@gmail.com>
2026-02-23 18:55:15 +0530

    Fix import case for userDataContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Customize from "./pages/Customize";
 import { useContext } from "react";
-import { userDataContext } from "./context/userContext";
+import { userDataContext } from "./context/UserContext";
 import Home from "./pages/Home";
 import Customize2 from "./pages/Customize2";
 
